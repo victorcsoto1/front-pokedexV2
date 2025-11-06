5e7ec32fdad4175d00e7dcc7ac9070e0de91a5c0
victorcsoto1 <victor.c.soto.09@gmail.com>
2025-11-06 19:00:39 -0300

    Se agregan iconos en menu de header en homein

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ import "../../styles/Header.css"
 import pokeapiLogo from "../../assets/logo_pokeapi.webp"
 import { useNavigate } from "react-router-dom"
 import { useAuth } from "../auth/AuthProvider";
-import { IconUser } from "../../assets/icons";
+import { IconLogout, IconUser, IconUserFilled } from "../../assets/icons";
 
 export default function Header () {
 
@@ -26,8 +26,8 @@ export default function Header () {
                             <div className="profile__dropdown dropdown">
                                 <span className="dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false"><IconUser color={"#A7A7A7"}/><span className="ms-3">Hola {usuario.decoded.nombre + " "}</span></span>
                                 <ul className="dropdown-menu">
-                                    <li className="dropdown-item" onClick={() => goToPage('/pokedex/edit-profile')}>Editar perfil</li>
-                                    <li className="dropdown-item" onClick={logout}>Cerrar sesión</li>
+                                    <li className="dropdown-item" onClick={() => goToPage('/pokedex/edit-profile')}><IconUserFilled color={"#A7A7A7"} className="pe-1"/>Editar perfil</li>
+                                    <li className="dropdown-item" onClick={logout}><IconLogout color={"#A7A7A7"} className="pe-1" />Cerrar sesión</li>
                                 </ul>
                             </div>
                         </div>
